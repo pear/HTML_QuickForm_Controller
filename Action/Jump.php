c3da0daeb0654c1d366c1f4696b89b7f81f3ee20
Alexey Borzov <avb@php.net>
2008-07-22 11:05:20 +0000

    Need absolute URI in Location header (bug #13087)
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_QuickForm_Controller/trunk@263173 c90b9560-bf6c-de11-be94-00142212c4b1

--- Action/Jump.php
+++ Action/Jump.php
@@ -3,7 +3,7 @@
 
 /**
  * This action performs HTTP redirect to a specific page.
- * 
+ *
  * PHP versions 4 and 5
  *
  * LICENSE: This source file is subject to version 3.01 of the PHP license
@@ -28,7 +28,7 @@ require_once 'HTML/QuickForm/Action.php';
 
 /**
  * This action performs HTTP redirect to a specific page.
- * 
+ *
  * @category    HTML
  * @package     HTML_QuickForm_Controller
  * @author      Alexey Borzov <avb@php.net>
@@ -36,6 +36,100 @@ require_once 'HTML/QuickForm/Action.php';
  */
 class HTML_QuickForm_Action_Jump extends HTML_QuickForm_Action
 {
+   /**
+    * Splits (part of) the URI into path and query components
+    *
+    * @param    string  String of the form 'foo?bar'
+    * @return   array   Array of the form array('foo', '?bar)
+    * @access   private
+    */
+    function _splitUri($uri)
+    {
+        if (false === ($qm = strpos($uri, '?'))) {
+            return array($uri, '');
+        } else {
+            return array(substr($uri, 0, $qm), substr($uri, $qm));
+        }
+    }
+
+   /**
+    * Removes the '..' and '.' segments from the path component
+    *
+    * @param    string  Path component of the URL, possibly with '.' and '..' segments
+    * @return   string  Path component of the URL with '.' and '..' segments removed
+    * @access   private
+    */
+    function _normalizePath($path)
+    {
+        $pathAry = explode('/', $path);
+        $i       = 1;
+
+        do {
+            if ('.' == $pathAry[$i]) {
+                if ($i < count($pathAry) - 1) {
+                    array_splice($pathAry, $i, 1);
+                } else {
+                    $pathAry[$i] = '';
+                    $i++;
+                }
+
+            } elseif ('..' == $pathAry[$i] && $i > 1 && '..' != $pathAry[$i - 1]) {
+                if ($i < count($pathAry) -1) {
+                    array_splice($pathAry, $i - 1, 2);
+                    $i--;
+                } else {
+                    array_splice($pathAry, $i - 1, 2, '');
+                }
+
+            } else {
+                $i++;
+            }
+        } while ($i < count($pathAry));
+
+        return implode('/', $pathAry);
+    }
+
+   /**
+    * Resolves relative URL using current page's URL as base
+    *
+    * The method follows procedure described in section 4 of RFC 1808 and
+    * passes the examples provided in section 5 of said RFC. Values from
+    * $_SERVER array are used for calculation of "current URL"
+    *
+    * @param    string  Relative URL, probably from form's action attribute
+    * @return   string  Absolute URL
+    * @access   private
+    */
+    function _resolveRelativeURL($url)
+    {
+        $https  = !empty($_SERVER['HTTPS']) && ('off' != $_SERVER['HTTPS']);
+        $scheme = ($https? 'https:': 'http:');
+        if ('//' == substr($url, 0, 2)) {
+            return $scheme . $url;
+
+        } else {
+            $host   = $scheme . '//' . $_SERVER['SERVER_NAME'] .
+                      (($https && 443 == $_SERVER['SERVER_PORT'] ||
+                        !$https && 80 == $_SERVER['SERVER_PORT'])? '': ':' . $_SERVER['SERVER_PORT']);
+            if ('' == $url) {
+                return $host . $_SERVER['REQUEST_URI'];
+
+            } elseif ('/' == $url[0]) {
+                return $host . $url;
+
+            } else {
+                list($basePath, $baseQuery) = $this->_splitUri($_SERVER['REQUEST_URI']);
+                list($actPath, $actQuery)   = $this->_splitUri($url);
+                if ('' == $actPath) {
+                    return $host . $basePath . $actQuery;
+                } else {
+                    $path = substr($basePath, 0, strrpos($basePath, '/') + 1) . $actPath;
+                    return $host . $this->_normalizePath($path) . $actQuery;
+                }
+            }
+        }
+    }
+
     function perform(&$page, $actionName)
     {
         // check whether the page is valid before trying to go to it
@@ -54,6 +148,10 @@ class HTML_QuickForm_Action_Jump extends HTML_QuickForm_Action
         }
         // generate the URL for the page 'display' event and redirect to it
         $action = $current->getAttribute('action');
+        // Bug #13087: RFC 2616 requires an absolute URI in Location header
+        if (!preg_match('!^https?://!i', $action)) {
+            $action = $this->_resolveRelativeURL($action);
+        }
         $url    = $action . (false === strpos($action, '?')? '?': '&') .
                   $current->getButtonName('display') . '=true' .
                   ((!defined('SID') || '' == SID || ini_get('session.use_only_cookies'))? '': '&' . SID);
